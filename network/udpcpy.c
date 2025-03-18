68bf863914ad4ec45eceddfc031bbb0359369494
Moutanazhir <adam.errais07@gmail.com>
2025-03-18 17:12:59 +0100

    Update udpcpy.c

--- network/udpcpy.c
+++ network/udpcpy.c
@@ -1,33 +1,32 @@
 #include "includes/networking.h"
+#include "networking.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define PYTHON_IP "127.0.0.1"
-#define PYTHON_PORT 5005
+/*udp C -> Python*/
 
-/*le code pour le transfert UDP de l'evenement réseau de la partie C vers Python*/
 
-struct sockaddr_in python_sa;
-int socketfd;
+int main() {
+    struct sockaddr_in python_sa;
+    int socketfd;
+    socketfd = udpclient(&python_sa, SERVERPORT, LOCALHOSTIP);
+    if (socketfd < 0) {
+        stop("Échec de la création de socket UDP");
+    }
 
-socketfd = udpclient(&python_sa, PYTHON_PORT, PYTHON_IP);
-if (socketfd < 0) {
-    stop("Échec de la création du client UDP");
-}
+    printf("UDP démarré. Préparation à envoyer un message...\n");
 
-printf("Client UDP démarré. Préparation à envoyer un message...\n");
+    char message[BUFFER_SIZE] = "unit_move|playerA|10,20"; //message modifiable
+    socklen_t python_len = sizeof(python_sa);
+    int sentLen = sendto(socketfd, message, strlen(message), 0, (struct sockaddr *)&python_sa, python_len);
+    if (sentLen < 0) {
+        perror("Erreur lors de l'envoi des données");
+        close(socketfd);
+        exit(EXIT_FAILURE);
+    }
 
-char message[BUFFER_SIZE] = "unit_move|playerA|10,20"; /*là on modifiera le message selon celui reçue
-                                                        de la connection udp avec l'autre joueur*/
-
-socklen_t python_len = sizeof(python_sa);
-int sentLen = sendto(socketfd, message, strlen(message), 0, (struct sockaddr *)&python_sa, python_len);
-if (sentLen < 0) {
-    perror("Erreur lors de l'envoi des données");
+    printf("Message envoyé à la partie Python: %s\n", message);
     close(socketfd);
-    exit(EXIT_FAILURE);
+    return 0;
 }
-
-printf("Message envoyé à la partie Python: %s\n", message);
-close(socketfd);
