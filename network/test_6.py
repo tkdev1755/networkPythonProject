e263f942f8e6375df1750a4102ff1ad76ba86183
maxgigi <max.grdt@gmail.com>
2025-03-18 14:53:15 +0100

    test_6.py on peut maintenant s'envoyer mutuellement des trucs

--- network/test_6.py
+++ network/test_6.py
@@ -3,13 +3,10 @@ import time
 
 tab=[1,0,0,0]
 
-def deplacer(i): 
-    if i+1 >3 :
-        tab[i] = 0
-        tab[0] = 1
-    else :
-        tab[i] = 0 
-        tab[i+1]=1
+def deplacer(i):
+    global tab
+    tab=[0,0,0,0]
+    tab[i]=1
 
 def decoupe(string):
     return string.split(";")
@@ -19,6 +16,8 @@ count=0
 UDP_IP = "192.168.128.254"
 UDP_PORT = 5005
 
+IP_ETAN = "192.168.128.250"
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.setblocking(0) #socket non bloquante
@@ -26,9 +25,11 @@ sock.setblocking(0) #socket non bloquante
 sock.bind((UDP_IP,UDP_PORT))
 
 while True:
+    sock.sendto(b"ajouter;1", (IP_ETAN, UDP_PORT))
+
     try:
         data, addr = sock.recvfrom(1024)
-        #print("received message: %s" % data))
+        print("received message: %s" % data)
         fun,val = decoupe(data.decode('utf-8'))
         
         if fun == "deplacer" and val.isdigit():
@@ -37,7 +38,7 @@ while True:
     except BlockingIOError:
         pass
 
-    print("count:",count)
+    print("count:",count,end=" ")
     count+=1
     print(tab)
     time.sleep(1)
