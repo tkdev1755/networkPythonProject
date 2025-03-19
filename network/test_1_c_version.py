52bfc092e466e0aa01cddda578b905ccadaeca77
etan-test1 <etandu0627@gmail.com>
2025-03-19 17:42:56 +0100

    needed 2

--- network/test_1_c_version.py
+++ network/test_1_c_version.py
@@ -3,44 +3,48 @@ import time
 import pickle
 import os
 import subprocess
+import utils
+import asyncio
+from utils import NetworkEngine
+LOCALHOSTIP  = "127.0.0.1"
+LOCALHOSTPORT = 5005
 
-"""
-print("Créer une partie 'c' ou  rejoindre une partie 'r' \n")
-corj = input()
+def intializeProgramSocket():
+    print("NetworkEngine init START")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((LOCALHOSTIP,LOCALHOSTPORT))
+    sock.setblocking(True)
+    print("Socket opened")
+    return sock
 
-if corj == 'r' :
-    print("Entrée une adresse IP à join\n")
-    ip = input()
-    print("Entrée un port \n")
-    port = input()
-
-else :
-    print("Entrée un port \n")
-    port = input()
-
-
-
-
-"""
-
-    #pas thread non blockan
-
-UDP_IP_NOT_ME = "192.168.128.250" #192.168.128.254
-UDP_PORT = 5005
-UDP_IP_ME = "192.168.128.254" #192.168.128.250
-
-
-#MESSAGE = b"Hello, World!"
-sock = socket.socket(socket.AF_INET, # Internet
-                      socket.SOCK_DGRAM) # UDP
-
-sock.bind((UDP_IP_ME,UDP_PORT))
-sock.setblocking(0) #socket non bloquante
-
-global compteur
-compteur = 0
-tab = [1,0,0,0]
-
+def programHandshake(programSocket):
+    print("Started Handshake")
+    try: 
+            data, addr = programSocket.recvfrom(20)
+            print("ADDR IS ", addr)
+            tempADDR = addr
+            print(data)
+            print("RECIEVED SOMETHING !")
+            recievedData = data.decode('utf-8')
+            command,id,port = decoupe(recievedData)
+            if (command == "PROG_CONNECT_OK"):
+                print("PROGINIT OK")
+                try:
+                    programSocket.sendto(bytes(f"OK_200; ; ",'utf-8'),addr)
+                    print("Network init END")
+                    print("ADDR IS ", tempADDR)
+                    return addr
+                except Exception as e :
+                    print("ERREUR ?")
+                    return -1
+                    pass
+    except BlockingIOError as e:
+        print("Waiting to recieve DATA")
+        pass
+    except Exception as e:
+        print("ERROR !! : ",e)
+        
+        
 def ajouter(i):
     global compteur
     compteur = compteur + i 
@@ -66,26 +70,69 @@ def get_file_size(file_path):
         print("Le fichier n'existe pas.")
         return None
     
+##### BOUCLE INITIALISATION #####
+
+print("Créer une partie 'n' ou  rejoindre une partie 'j' \n")
+
+norj = input()
+DEST_IP = "NONE"
+DEST_PORT = -1
+if norj == 'j' :
+    print("Entrez une adresse IP à join\n")
+    DEST_IP = input()
+    print("Entrez un port \n")
+    DEST_PORT = input()
+
+else :
+    print("Entrez un port \n")
+    port = input()
+
+
+programSocket = intializeProgramSocket()
+
+
+networkEngine = NetworkEngine()
+
+process = networkEngine.launch_c_program("./network/networkEngine",["j",f"{DEST_IP}", f"{DEST_PORT}"])
+
+programADDR = programHandshake(programSocket)
+
+
+print("Ended Program Handshake")
+if programADDR == -1: 
+    print("ERREUR lors de la récéption du message de la part du C")
+
+##########################
+## FIN INIT
+##########################
+
+    #pas thread non blockan
+
+"""UDP_IP_NOT_ME = "192.168.128.250" #192.168.128.254
+UDP_PORT = 5005
+UDP_IP_ME = "192.168.128.254" #192.168.128.250"""
+
+
+compteur = 0
+tab = [1,0,0,0]
+
+
 # LANCEMENT PROGRAMME C
 
-#subprocess.run(["./network/networkEngine", "n"])
 
+try:
+    data, addr = programSocket.recvfrom(1024)
+    global cPort 
+    cPort = addr[1]
+    print("received message: %s" % data)
+    print(addr)
+    fun,ip,port = decoupe(data.decode('utf-8'))
+    
+    if fun == "CONNECT" :
+        start = 1
 
-start = 0
-while not(start) : 
-    try:
-            data, addr = sock.recvfrom(1024)
-            global cPort 
-            cPort = addr[1]
-            print("received message: %s" % data)
-            print(addr)
-            fun,ip,port = decoupe(data.decode('utf-8'))
-            
-            if fun == "CONNECT" :
-                start = 1
-
-    except BlockingIOError:
-        pass
+except BlockingIOError:
+    pass
 
 # Envoie size tab au prog c
 
@@ -94,39 +141,46 @@ pickle.dump(tab,file)
 file.close
 
 file = open('save','rb')
+"""
 message = file.read(get_file_size('save'))
-sock.sendto(message,(UDP_IP_NOT_ME, UDP_PORT)) #cPort
+programSocket.sendto(message,(LOCALHOSTIP, cPort)) #cPort
+"""
+
+# Envoie taille save pickle
+programSocket.sendto(bytes("ACCEPT; ; ",'utf-8'),(LOCALHOSTIP, cPort)) #cPort
 
 
 # attendre de recevoir le GAME_STARTED
 
-start = 0
-while not(start) : 
-    try:
-            data, addr = sock.recvfrom(1024)
-            print("received message: %s" % data)
-            fun,ip,port = decoupe(data.decode('utf-8'))
-            
-            if fun == "START_GAME" :
-                start = 1
-                sock.sendto(bytes(f"GAME_STARTED;;",'utf-8'), (UDP_IP_NOT_ME,  UDP_PORT)) #cPort
+try:
+    data, addr = programSocket.recvfrom(1024)
+    print("received message: %s" % data)
+    fun,ip,port = decoupe(data.decode('utf-8'))
+                
+    if fun == "START_GAME" :
+        start = 1
+        #programSocket.sendto(bytes(f"GAME_STARTED;;",'utf-8'), (LOCALHOSTIP,  cPort)) #cPort
+
+    else :
+        print("ERROR WHILE STARTING THE GAME")
+        raise Exception ("Problème à l'allumage")
+                
+
+except BlockingIOError:
+    pass
 
 
-
-    except BlockingIOError:
-        pass
-
-
-
+programSocket.setblocking(0)
 while True :
+    
     time.sleep(1)
     i = tab.index(1)
     deplacer(i)
     print(tab)
-    sock.sendto(bytes(f"deplacer;{i}",'utf-8'), (UDP_IP_NOT_ME, UDP_PORT))
+    programSocket.sendto(bytes(f"deplacer;{i}",'utf-8'), (LOCALHOSTIP, cPort))
 
     try:
-        data, addr = sock.recvfrom(1024)
+        data, addr = programSocket.recvfrom(1024)
         print("received message: %s" % data)
         fun,val = decoupe(data.decode('utf-8'))
         
@@ -138,9 +192,11 @@ while True :
         pass
     print(compteur)
     
+"""
+if norj == n : 
+    #envoie la map
 
-print("UDP target IP: %s" % UDP_IP)
-print("UDP target port: %s" % UDP_PORT)
-print("message: %s" % MESSAGE)
-
-
+else :
+    #reçois la map 
+    #construit le monde
+"""
