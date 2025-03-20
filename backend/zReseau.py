3d14b1a1ed04f2fc1b684d00f1520cecc6a5cbb0
maxgigi <max.grdt@gmail.com>
2025-03-20 10:56:03 +0100

    zReseau contient les fonctions nécessaires à l'interprétation de messages "Action;ID;Data"

---
+++ backend/zReseau.py
@@ -0,0 +1,22 @@
+#fonctions permettant l'interprétation de messages "Action;ID;Data"
+#messages de trois formes:
+#"Set;ID;Data" demande la modification de l'état de l'objet d'id ID
+
+
+
+
+
+
+
+
+#fonction pour créer un message
+def create_message(action, id, data):
+    pass
+
+#fonction pour envoyer un message par réseau
+def send_message(message):
+    pass
+
+#fonction pour interpréter un message
+def interpret_message(message):
+    pass
