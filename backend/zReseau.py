9ade9a38bb8064a54e908f1275bc0365500bab4f
maxgigi <max.grdt@gmail.com>
2025-03-20 11:53:46 +0100

    zReseau move_by_id

--- backend/zReseau.py
+++ backend/zReseau.py
@@ -18,11 +18,20 @@ def create_message(action, id, data):
 def send_message(message,ip):
     pass
 
-#fonction pour interpréter un message
+#fonction pour recevoir un message par réseau ?
+def receive_message(): #peut être qu'il faudra mettre un argument pour l'ip ?
+    pass
+
+#fonction pour interpréter un message 
 def interpret_message(message):
     action, id, data=message.split(";")
     if action=="Set":
         move_by_id(id, data)
 
-def move_by_id(id,data):
-
+#fonction pour déplacer un objet par son id, IL FAUDRA LA METTRE EN METHODE DE LA CLASSE GAME_ENGINE
+def move_by_id(id,position): #juste, remplace la position de l'unité d'id id par position 
+    for player in self.players: #cherche l'unité d'id id et s'arrête une fois trouvée
+        for unit in player.units:
+            if unit.id==id:
+                unit.position=position
+                return
