8105231022f0677a21745d3b87e089566aafc186
etan-test1 <etandu0627@gmail.com>
2025-03-20 10:57:04 +0100

    begin change main and mod_game_engine

---
+++ backend/Mod_Game_Engine.py
@@ -0,0 +1,318 @@
+import curses
+import time
+import pickle
+import os
+import tkinter as tk
+
+from queue import Queue
+from frontend.gui import GUI
+
+from logger import debug_print
+from Units import *
+from Building import *
+from Actions import *
+from frontend.Terrain import Map
+try:
+    from frontend import gui
+    USE_PYGAME = True
+except ImportError:
+    USE_PYGAME = False
+    debug_print("Pygame not installed; running without Pygame features such as 2.5D map view.")
+
+from html_report import generate_html_report
+
+from IA import IA
+
+from zReseau import *
+
+# GameEngine Class
+class Mod_GameEngine:
+    def __init__(self, game_mode, map_size, players, sauvegarde=False):
+        self.game_mode = game_mode
+        self.map_size = map_size
+        self.players = players
+        self.map = Map(*map_size)  # Create a map object
+        self.turn = 0
+        self.is_paused = False  # Flag to track if the game is paused
+        self.changed_tiles = set()  # Set to track changed tiles
+        
+
+        # Sauvegarde related attributes
+        if not sauvegarde:
+            Building.place_starting_buildings(self.map)   # Place starting town centers on the map
+            Unit.place_starting_units(self.players, self.map)  # Place starting units on the map
+        
+        self.debug_print = debug_print
+        self.current_time = time.time()
+
+        self.terminalon = True
+
+        # GUI thread related attributes
+        self.gui_running = False
+        self.data_queue = Queue()
+        self.gui_thread = None
+
+    def start_gui_thread(self):
+        """Initialize and start the GUI thread"""
+        if not self.gui_thread:
+            self.data_queue = Queue()
+            self.gui_thread = GUI(self.data_queue)
+            self.gui_thread.start()
+            self.gui_running = True
+
+    def stop_gui_thread(self):
+        """Stop the GUI thread safely"""
+        if self.gui_thread:
+            #self.gui_thread.stop()
+            self.gui_thread = None
+            self.gui_running = False
+
+    def update_gui(self):
+        """Send current game state to GUI thread"""
+        if self.gui_running and not self.data_queue.full():
+            self.data_queue.put(self)
+
+    def get_current_time(self):
+        """Retourne le temps actuel si le jeu n'est pas en pause"""
+        if not self.is_paused:
+            return time.time()
+        return self.current_time
+
+    def run(self, stdscr):
+        # Initialize the starting view position
+        top_left_x, top_left_y = 0, 0
+        viewport_width, viewport_height = 30, 30
+
+        ######
+        # SOCKET ??????????
+        ######
+
+        # Display the initial viewport
+        stdscr.clear()  # Clear the screen
+        
+        if self.terminalon :
+            self.map.display_viewport(stdscr, top_left_x, top_left_y, viewport_width, viewport_height, Map_is_paused=self.is_paused)  # Display the initial viewport
+
+        try:
+            while not self.check_victory():
+                # Mettre à jour current_time au début de chaque itération si le jeu n'est pas en pause
+                if not self.is_paused:
+                    self.current_time = time.time()
+
+                # Handle input
+                curses.curs_set(0)  # Hide cursor
+                stdscr.nodelay(True)  # Make getch() non-blocking
+                key = stdscr.getch()  # Get the key pressed by the user
+                action = Action(self.map)
+
+                if key == curses.KEY_UP or key == ord('z'):
+                    top_left_y = max(0, top_left_y - 1)
+                elif key == curses.KEY_DOWN or key == ord('s'):
+                    top_left_y = min(self.map.height - viewport_height, top_left_y + 1)
+                elif key == curses.KEY_LEFT or key == ord('q'):
+                    top_left_x = max(0, top_left_x - 1)
+                elif key == curses.KEY_RIGHT or key == ord('d'):
+                    top_left_x = min(self.map.width - viewport_width, top_left_x + 1)
+                elif key == ord('Z'):
+                    top_left_y = max(0, top_left_y - 5)
+                elif key == ord('S'):
+                    top_left_y = min(self.map.height - viewport_height, top_left_y + 5)
+                elif key == ord('Q'):
+                    top_left_x = max(0, top_left_x - 5)
+                elif key == ord('D'):   
+                    top_left_x = min(self.map.width - viewport_width, top_left_x + 5)
+
+                ###### TEST KEYS #######
+
+                elif key == ord('r'):
+                    self.debug_print(self.players[0].buildings[0].position)
+                    self.debug_print(action.get_adjacent_positions(self.players[0].buildings[0].position[0], self.players[0].buildings[0].position[1], self.players[0].buildings[0].size))
+                elif key == ord('o'):
+                    self.terminalon = not self.terminalon   
+                elif key == ord('i'):
+                    action.construct_building(self.players[2].units[1], Keep, 10, 10, self.players[2], self.get_current_time())
+                    action.move_unit(self.players[1].units[1],15,15,self.get_current_time())
+                elif key == ord('u'):
+                    Building.kill_building(self.players[0], self.players[0].buildings[0], self.map)
+                elif key == ord('y'):
+                    Building.kill_building(self.players[0], self.players[0].buildings[-1], self.map)
+                elif key == ord('t'):
+                    self.players[0].owned_resources["Wood"] = 100
+
+
+                #########################
+
+                ###### CHEAT KEYS #######
+
+                elif key == ord('g'):
+                    self.players[0].owned_resources["Gold"] += 5000
+                elif key == ord('w'):
+                    self.players[0].owned_resources["Wood"] += 5000
+                elif key == ord('f'):
+                    self.players[0].owned_resources["Food"] += 5000
+                elif key == ord('h'):
+                    for player in self.players:
+                        player.owned_resources["Gold"] = 0
+                        player.owned_resources["Wood"] = 0
+                        player.owned_resources["Food"] = 0
+
+                #########################
+
+
+                elif key == curses.KEY_F9:
+                    if not self.gui_running:
+                        self.start_gui_thread()
+                    else:
+                        self.stop_gui_thread()
+                        stdscr.clear()
+                        stdscr.refresh()
+                        continue
+                elif key == ord('\t'):  # TAB key
+                    generate_html_report(self.players)
+                    self.debug_print(f"HTML report generated at turn {self.turn}")
+                    if self.is_paused == False:
+                        self.is_paused = True
+                        self.debug_print("Game paused.")
+                elif key == ord('p'):
+                    self.is_paused = not self.is_paused
+                    if self.is_paused:
+                        self.debug_print("Game paused.")
+                    else:
+                        self.debug_print("Game resumed.")
+
+                elif key == ord('n'):
+                    self.IA_used = not self.IA_used
+                    self.debug_print(f"IA used: {self.IA_used}")
+
+                elif key == curses.KEY_F10: 
+                    self.save_game()
+                elif key == curses.KEY_F12:
+                    # Find the latest save file in the directory
+                    save_dir = os.path.join(os.path.dirname(__file__), '..', 'assets', 'annex')
+                    save_files = [f for f in os.listdir(save_dir) if f.endswith('.dat')]
+                    if save_files:
+                        latest_save_file = max(save_files, key=lambda f: os.path.getctime(os.path.join(save_dir, f)))
+                        latest_save_path = os.path.join(save_dir, latest_save_file)
+                        self.load_game(latest_save_path)
+                    else:
+                        self.debug_print("No save files found.")
+
+
+                ###########
+                ## IA WAS HERE
+                ############
+
+
+                    
+                if not self.is_paused and self.turn % 10 == 0:
+                    # Move units toward their target position
+                    """try:
+                        data, addr = sock.recvfrom(1024)
+                        print("received message: %s" % data)
+                        interpret_message(data.decode('utf-8'))
+
+                    except BlockingIOError:
+                        pass"""
+
+                # Clear the screen and display the new part of the map after moving
+                stdscr.clear()
+                if self.terminalon :
+                    self.map.display_viewport(stdscr, top_left_x, top_left_y, viewport_width, viewport_height, Map_is_paused=self.is_paused)
+                stdscr.refresh()
+
+                if self.gui_running:
+                    self.update_gui()
+
+                self.turn += 1
+
+            active_players = [p for p in self.players if p.units or p.buildings]
+            self.debug_print(f"Player {active_players[0].name} wins the game!", 'Magenta')
+            input("Press Enter to exit...")
+
+        except KeyboardInterrupt:
+            self.debug_print("Game interrupted. Exiting...", 'Yellow')
+        finally:
+            if self.gui_running:
+                self.stop_gui_thread()
+
+    def check_victory(self):
+        if self.turn % 500 == 0: # Check if the game is over
+            active_players = [p for p in self.players if p.units or p.buildings] # Check if the player has units and buildings
+            return len(active_players) == 1 # Check if there is only one player left
+        else:
+            return False
+
+    #condition de victoire: être le dernier joueur avec des bâtiments
+    def victory():
+    
+        def big_text(text):
+            root = tk.Tk()
+            root.title("Texte en gros")
+            label = tk.Label(root, text=text, font=("Arial", 48), padx=20, pady=20)
+            label.pack(expand=True)
+            root.mainloop()
+
+        if Mod_GameEngine.check_victory(Mod_GameEngine.self) ==True:
+            time.stop()
+            GUI.load_image(GUI.IMG_PATH / "victory.png")
+            big_text(f"Player {Mod_GameEngine.active_players[0].name} wins the game!")
+
+    def pause_game(self):
+        self.is_paused = not self.is_paused
+
+    def save_game(self, filename=None):
+        if not self.is_paused:
+            self.is_paused = True
+            self.debug_print("Game paused.")
+        
+        # Generate a filename if none is provided
+        if filename is None:
+            for i in range(10):  # Limit to 10 auto-saves
+                filename = f"../assets/annex/game_save{i}.dat"
+                if not os.path.exists(filename):  # Check if the file exists
+                    break
+            else:
+                self.debug_print("No available slots to save the game.")
+                return
+        else:
+            # If a filename is provided, add a unique suffix if necessary
+            base, ext = os.path.splitext(filename)
+            counter = 1
+            while os.path.exists(filename):
+                filename = f"{base}_{counter}{ext}"
+                counter += 1
+
+        # Save the game state
+        try:
+            with open(filename, 'wb') as f:
+                game_state = {
+                    'players': self.players,
+                    'map': self.map,
+                    'turn': self.turn,
+                    'is_paused': self.is_paused,
+                    'changed_tiles': self.changed_tiles,
+                    'ias': self.ias  # Add self.ias to the saved state
+                }
+                pickle.dump(game_state, f)
+            self.debug_print(f"Game saved to {filename}.")
+        except Exception as e:
+            self.debug_print(f"Error saving game: {e}")
+
+
+    def load_game(self, filename):
+        if not self.is_paused:
+            self.is_paused = True
+            self.debug_print("Game paused.")
+        try:
+            with open(filename, 'rb') as f:
+                game_state = pickle.load(f)
+                self.players = game_state['players']
+                self.map = game_state['map']
+                self.turn = game_state['turn']
+                self.is_paused = game_state['is_paused']
+                self.changed_tiles = game_state['changed_tiles']
+                self.ias = game_state.get('ias', None)  # Load self.ias or set it to None if missing
+                self.current_time = time.time()
+            self.debug_print(f"Game loaded from {filename}.")
+        except Exception as e:
+            self.debug_print(f"Error loading game: {e}")
--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,62 @@
 import os
 import sys
 import argparse
-
+import curses
 import config
+from Players import Player
+#from Mod_Game_Engine import Mod_GameEngine
 
-# Ajouter le chemin du projet à sys.path pour ne pas avoir à le faire dans le terminal
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(project_root)
+norj = input("Tape n pour créer une partie et j pour join :")
 
-from backend.Starter_File import start_menu
+if norj =='n' :
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Launch game with or without save and debug mode.")
-    parser.add_argument(
-        "-s", "--save", 
-        type=str, 
-        help="Path to save file (optional)."
-    )
-    parser.add_argument(
-        "-d", "--debug", 
-        action="store_true", 
-        default=False, 
-        help="Enable debug mode (default=False)."
-    )
-    args = parser.parse_args()
-    config.debug_mode = args.debug
-    start_menu(save_file=args.save)
+    # Ajouter le chemin du projet à sys.path pour ne pas avoir à le faire dans le terminal
+    project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    sys.path.append(project_root)
+
+    from backend.Starter_File import start_menu
+
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser(description="Launch game with or without save and debug mode.")
+        parser.add_argument(
+            "-s", "--save", 
+            type=str, 
+            help="Path to save file (optional)."
+        )
+        parser.add_argument(
+            "-d", "--debug", 
+            action="store_true", 
+            default=False, 
+            help="Enable debug mode (default=False)."
+        )
+        args = parser.parse_args()
+        config.debug_mode = args.debug
+        start_menu(save_file=args.save)
+
+else :
+    players = []
+    GameMode = "Utopia"
+    map_size = (120,120)
+    num_players = 2
+    player1 = Player(
+                        f'Player 1',
+                        None,
+                        None,
+                        player_id=1
+                    )
+    player2 = Player(
+                        f'Player 2',
+                        None,
+                        None,
+                        player_id=2
+                    )
+    players.append(player1)
+    players.append(player2)
+    curses.wrapper(lambda stdscr: Mod_GameEngine(
+                    game_mode=GameMode,
+                    map_size=map_size,
+                    players=players,
+                    sauvegarde=False
+                ).run(stdscr))
+    
+
