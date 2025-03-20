519d6f98b83098ed9f427603b8787c71303c5a63
maxgigi <max.grdt@gmail.com>
2025-03-20 10:41:23 +0100

    Units.py ai rajouté les ids aux unités, dans la branche solo_player

--- backend/Units.py
+++ backend/Units.py
@@ -25,6 +25,10 @@ class Unit:
         self.current_frame = 0  # Initialiser à 0 si absent
         self.frame_counter = 0 
         self.is_moving = False
+        self.id = None
+
+    def id_maker(self):
+        self.id = random.randint(0, 999999999)
 
     def __str__(self):
         return self.symbol  # Ensure the building is represented by just the symbol
